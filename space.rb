fe78782a7ebb71e59f2b58d20152298b6fb90800
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-08-06 11:52:33 +0300

    Add functionalities to the game

--- space.rb
+++ space.rb
@@ -13,6 +13,28 @@ class Space < Gosu::Window
     end
 
     def update
+        if Gosu::button_down? Gosu::KB_LEFT or Gosu::button_down? Gosu::GP_LEFT
+            @player.turn_left
+        end
+        if Gosu::button_down? Gosu::KB_RIGHT or Gosu::button_down? Gosu::GP_RIGHT
+            @player.turn_right
+        end
+        if Gosu::button_down? Gosu::KB_UP or Gosu::button_down? Gosu::GP_BUTTON_0
+            @player.accelerate
+        end
+        @player.move
+        # player.collect_stars(stars)
+        # if rand(100) < 4 and stars.size < 25
+        #     stars.push(Star.new)
+        # end
+    end
+
+    def button_down?
+        if id == Gosu::KB_ESCAPE
+            close
+        else
+            super
+        end
     end
 
     def draw
